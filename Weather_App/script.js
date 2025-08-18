9cfedc0af0df058582ea0456dd74908ed634b5a1
ManishDethe4 <manishdethe02@gmail.com>
2025-08-18 12:30:46 +0530

    Updates

---
+++ Weather_App/script.js
@@ -0,0 +1,40 @@
+const cityInput = document.querySelector('.city-input');
+const searchBtn = document.querySelector('.search-btn');
+const weatherInfoSection = document.querySelector('.weather-info')
+const notFoundSection = document.querySelector('.not-found')
+const searchCitySection = document.querySelector('.search-city')
+const apiKey = '501dad2e24c9593f0fa937a498e293d2'
+
+searchBtn.addEventListener('click', () => {
+    if(cityInput.value.trim() != '') {
+        updateWeatherInfo(cityInput.value)
+        cityInput.value = ''
+        cityInput.blur()
+    }
+})
+cityInput.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter' &&
+        cityInput.value.trim() != ''
+    ) {
+        updateWeatherInfo(cityInput.value)
+        cityInput.value = ''
+        cityInput.blur()
+    }
+})
+async function getFetchData(endPoint, city) {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/${endPoint}?q=${city}&appid=${apiKey}&units=metric`
+
+    const response = await fetch(apiUrl)
+    return response.json()
+}
+async function updateWeatherInfo(city) {
+    const weatherData = await  getFetchData('weather', city)
+    
+    if(weatherData.cod != 200){
+        showDisplaySection(notFoundSection)
+        return
+    }
+}
+function showDisplaySection(section) {
+
+}
